57d781409eb460ab09d523cdd75b72573cbc9e2e
mrocha <mrocha@debian>
2021-05-15 19:14:29 +0100

    Add Black as the linter

--- tzbot.py
+++ tzbot.py
@@ -24,34 +24,34 @@ class TZBot:
         return self._parse_msg(line)
 
     def _send_msg(self, result: str) -> None:
-        self.ostream.write(f'{result}\n')
+        self.ostream.write(f"{result}\n")
 
     def _is_a_message(self, line: str) -> bool:
         nick, _ = self._parse_msg(line)
         return self._is_valid_nick(nick) if nick else False
 
     def _parse_msg(self, line: str) -> List[Optional[str]]:
-        return line.split(': ', 1) if ': ' in line else [None, None]
+        return line.split(": ", 1) if ": " in line else [None, None]
 
     def _is_valid_nick(self, nick: str) -> bool:
-        return re.fullmatch(r'[a-zA-Z0-9]{1,32}', nick) is not None
+        return re.fullmatch(r"[a-zA-Z0-9]{1,32}", nick) is not None
 
     def _process_cmd(self, nick: str, msg: str) -> Optional[str]:
         cmd = msg.strip().split()
         cmd, args = cmd[:1], cmd[1:]
 
         # Ignore messages that are not commands
-        if not cmd or (cmd := cmd[0])[0] != '!':
+        if not cmd or (cmd := cmd[0])[0] != "!":
             return None
 
         # !timeat <tzinfo>
-        if cmd == '!timeat' and len(args) == 1:
+        if cmd == "!timeat" and len(args) == 1:
             if tztime := self._get_time_at(args[0]):
                 self._increment_popularity_of(args[0])
                 return self._format_time(tztime)
 
         # !timepopularity <tzinfo_or_prefix>
-        elif cmd == '!timepopularity' and len(args) == 1:
+        elif cmd == "!timepopularity" and len(args) == 1:
             return str(self._get_popularity_of(args[0]))
 
         return None
@@ -69,7 +69,7 @@ class TZBot:
         return 0
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     bot = TZBot(sys.stdin, sys.stdout)
     while True:
         bot.process_msgs()
